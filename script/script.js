fc301a47cc7e3a59553721e235851ca3df25d3ca
User8400 <vano_1@list.ru>
2020-12-26 03:04:59 +0300

    Новые темы
    Добавил темную и светлую темы, добавил метод обработку кликов по карточкам

--- script/script.js
+++ script/script.js
@@ -5,6 +5,62 @@ var elCard = document.querySelectorAll('.card');
 	console.log(elementCard.id);
 
 	elementCard.onclick = function(e) {
-		console.log(this);
+		//console.log(this);
+		//console.log(this.data_doc_id);
+		//alert(this.id)
+		elem = document.getElementById(this.id);
+		//console.log(elem)
+		data_doc_id = elem.getAttribute("data_doc_id");
+
+		console.log(elem.getAttribute("data_doc_id"));
+		console.log(elem.getAttribute("data_doc_key"));
+		//console.log(elem.data_doc_id)
 	}
 });
+
+window.onload = init;
+
+function init(){
+    /*var button = document.getElementById("ColTButton")
+    button.onclick = handleButtonColorStyle;
+    var button = document.getElementById("lightTButton")
+    button.onclick = handleButtonLightstyle;
+    var button = document.getElementById("darkTButton")
+    button.onclick = handleButtondarkstyle;*/
+}
+
+function handleButtonColorStyle() {
+    //alert("Цветной стиль");
+    /*
+    var elements = document.getElementsByClassName("bg-card-color"),i,len;
+    for (i = 0, len = elements.length; i < len; i += 1) {
+       console.log(elements[i]);
+       if (typeof elements[i] == "undefined") {}
+       else {
+       		elements[i].className = "card bg-card-light";
+       }
+    }
+	var elements = document.getElementsByClassName("bg-color"),i,len;
+    for (i = 0, len = elements.length; i < len; i += 1) {
+       console.log(elements[i]);
+       if (typeof elements[i] == "undefined") {}
+       else {
+       		elements[i].className = "header bg-light";
+       }
+    }
+	var elements = document.getElementsByClassName("bg-footer-color"),i,len;
+    for (i = 0, len = elements.length; i < len; i += 1) {
+       console.log(elements[i]);
+       if (typeof elements[i] == "undefined") {}
+       else {
+       		elements[i].className = "footer bg-footer-light";
+       }
+    }*/
+}
+function handleButtonLightstyle() {
+    //alert("Светлый стиль");
+}
+function handleButtondarkstyle() {
+    //alert("Темный стиль");
+}
+
